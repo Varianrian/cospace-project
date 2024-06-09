09e20ba58333978466671e60b7b7206d3f5e2cdb
Varian Avila F <varian236@gmail.com>
2024-06-09 16:51:38 +0700

    fix routes for api

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,7 @@ Route::post('/email/verification-notification', function (Request $request) {
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
-Route::group(['prefix' => 'api'], function () {
+Route::group(['prefix' => 'v1'], function () {
     Route::get('user', function (Request $request) {
         return $request->user();
     });
