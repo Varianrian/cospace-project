838b0aba173dd3da93faa717deae9233870119fb
adindaaasals <adindaaasals28@gmail.com>
2024-06-16 17:47:48 +0700

    add-ticket

--- resources/views/components/profile-booking.blade.php
+++ resources/views/components/profile-booking.blade.php
@@ -75,15 +75,38 @@
     <!-- Modal -->
   <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
-        <div class="relative bg-white rounded-lg shadow">
-            <div class="flex items-center justify-center p-4 md:p-5 border-b rounded-t">
-                <h3 class="text-xl font-semibold text-gray-900 ">
-                    CO&CO Hub
+        <div class="relative bg-white rounded-lg shadow p-4 md:p-5">
+            <div class="flex items-center justify-center">
+                <h3 class="text-[24px] font-semibold text-[#211951]">
+                    EduPlex Coworking Space
                 </h3>
             </div>
-            
-           
-            <div class="flex justify-center">
+            <div class="flex items-center justify-center  mt-4">
+                <h3 class="text-[24px] font-semibold text-[#211951]">
+                    INVOICE : p6YOEoZ5
+                </h3>
+            </div>
+            <div class="flex items-center justify-center mt-4">
+                <p class="text-[16px] font-normal text-[#112211]">
+                    Senin, 1 Desember 2024
+                </p>
+            </div>
+            <div class="flex items-center justify-center mt-4">
+                <div class="flex justify-start gap-16">
+                    <div class="flex gap-4 self-stretch">
+                        <div class="flex flex-col whitespace-nowrap">
+                        <div class="text-base text-[#112211]">Check-In</div>
+                        <div class="mt-2 text-xl font-medium text-center">13:00</div>
+                        </div>
+                        <div class="my-auto text-xl font-semibold">—</div>
+                        <div class="flex flex-col">
+                        <div class="text-base text-[#112211">Check Out</div>
+                        <div class="mt-2 text-xl font-medium text-center">17:00</div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-center mt-4">
               <button type="button" class="mr-4 py-2 px-4 mb-4 text-white bg-[#0021A3] rounded-lg text-sm  inline-flex justify-center items-center" data-modal-hide="authentication-modal">
                 Close
                 <span class="sr-only">Close modal</span>
