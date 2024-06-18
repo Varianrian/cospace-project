67e58d63791510438fc6648d2528c8fab3be373a
adindaaasals <adindaaasals28@gmail.com>
2024-06-18 22:49:53 +0700

    add href for kategori

--- resources/views/components/kategori.blade.php
+++ resources/views/components/kategori.blade.php
@@ -2,15 +2,15 @@
 <h5 class="xl:ml-12 lg:ml-12 font-bold text-[#000000] lg:text-[26px] sm:text-center">Kategori</h5>
 <div class="flex flex-wrap justify-center gap-6 pt-5">
   <figure class="image-text-container relative">
-    <a href="">
+    <a href="/workspace?search=coworking">
       <img class="h-[250px] w-[367px] rounded-t-lg" src="{{ asset('assets/images/kategori1.png') }}" alt="kategori1" />
     </a>
     <figcaption class="absolute bottom-6 left-1/2 transform -translate-x-1/2 text-center  font-[22px] text-white">
-      <p>Coworking (Shared Space)</p>
+      <p>Coworking (Private)</p>
     </figcaption>
   </figure>
   <figure class="image-text-container relative">
-    <a href="">
+    <a href="/workspace?search=meeting">
       <img class="h-[250px] w-[367px] rounded-t-lg" src="{{ asset('assets/images/kategori2.png') }}" alt="kategori2" />
     </a>
     <figcaption class="absolute bottom-6 left-1/2 transform -translate-x-1/2 text-center  font-[22px] text-white">
@@ -18,15 +18,15 @@
     </figcaption>
   </figure>
   <figure class="image-text-container relative">
-    <a href="">
+    <a href="/workspace?search=event">
       <img class="h-[250px] w-[367px] rounded-t-lg" src="{{ asset('assets/images/kategori3.png') }}" alt="kategori3" />
     </a>
-    <figcaption class="absolute bottom-6 left-1/2 transform -translate-x-1/2 text-center  font-[22px] text-white"">
+    <figcaption class="absolute bottom-6 left-1/2 transform -translate-x-1/2 text-center  font-[22px] text-white">
       <p>Event Hall</p>
     </figcaption>
   </figure>
   <figure class="image-text-container relative">
-    <a href="">
+    <a href="/workspace?search=studio">
       <img class="h-[250px] w-[367px] rounded-t-lg" src="{{ asset('assets/images/kategori4.png') }}" alt="kategori2" />
     </a>
     <figcaption class="absolute bottom-6 left-1/2 transform -translate-x-1/2 text-center  font-[22px] text-white">
@@ -34,7 +34,7 @@
     </figcaption>
   </figure>
   <figure class="image-text-container relative">
-    <a href="">
+    <a href="/workspace?search=private">
       <img class="h-[250px] w-[367px] rounded-t-lg" src="{{ asset('assets/images/kategori5.png') }}" alt="kategori2" />
     </a>
     <figcaption class="absolute bottom-6 left-1/2 transform -translate-x-1/2 text-center  font-[22px] text-white">
